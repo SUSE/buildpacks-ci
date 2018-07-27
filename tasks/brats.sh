a9d18992ff8b9ea334ac025902760504e3240df4
Tim Hardeck <thardeck@suse.de>
2018-07-27 14:18:46 +0200

    Also copy VERSION file from obs buildpack

--- tasks/brats.sh
+++ tasks/brats.sh
@@ -4,10 +4,13 @@ set -e
 
 source ci/tasks/cf_login.sh
 
+rm ruby-buildpack/manifest.yml ruby-buildpack/VERSION
+
 cd lftp.obs-buildpacks-staging
 rpm2cpio *.src.rpm | cpio -idmv
 tar xf v*.tar.gz
-mv cf-ruby-buildpack-*/manifest.yml ../ruby-buildpack/
+cp cf-ruby-buildpack-*/manifest.yml ../ruby-buildpack/
+cp cf-ruby-buildpack-*/VERSION ../ruby-buildpack/
 cd ../ruby-buildpack
 
 scripts/brats.sh
