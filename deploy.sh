59f20d2314d60cca4b8f580351520f94477677f3
Dimitris Karakasilis <DKarakasilis@suse.com>
2018-08-03 14:02:17 +0300

    Add deployment script
    Signed-off-by: André Duffeck <aduffeck@suse.com>

---
+++ deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+common() {
+	fly -t ${TARGET} set-pipeline -p ${2} -c <(erb ${1}/pipeline.yaml) -l ${CONCOURSE_SECRETS_FILE} && \
+	fly -t ${TARGET} unpause-pipeline -p ${2} && \
+	fly -t ${TARGET} expose-pipeline -p ${2}
+}
+
+brats() {
+  common brats buildpacks-brats
+}
+
+buildpacks() {
+  common buildpacks buildpacks
+}
+
+if test -n "${CONCOURSE_SECRETS_FILE:-}"; then
+    if test -r "${CONCOURSE_SECRETS_FILE:-}" ; then
+        secrets_file="${CONCOURSE_SECRETS_FILE}"
+    else
+        printf "ERROR: Secrets file %s is not readable\n" "${CONCOURSE_SECRETS_FILE}" >&2
+        exit 2
+    fi
+else
+    echo "ERROR: CONCOURSE_SECRETS_FILE location is not set" >&2
+    exit 3
+fi
+
+if [ -z "${TARGET}" ]; then
+	echo "ERROR: No TARGET specified. Please set the TARGET env variable"
+	exit 1
+fi
+
+case $1 in
+brats)
+	brats
+  ;;
+buildpacks)
+	buildpacks
+  ;;
+*)
+  echo "You didn't specify a pipeline to deploy. Available options: brats, buildpacks"
+	exit 1
+  ;;
+esac
