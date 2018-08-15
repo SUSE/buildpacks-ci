2960510ed99bf5a49f583e77c075eb4526855831
André Duffeck <aduffeck@suse.com>
2018-08-15 15:34:02 +0200

    Extract release pull request creation into a separate job
    Signed-off-by: Tim Hardeck <thardeck@suse.com>

---
+++ brats/tasks/create-release-pr.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -ex
+
+# Get rid of quotes in the beginning and end
+export GITHUB_PRIVATE_KEY=${GITHUB_PRIVATE_KEY:1:-1}
+
+# Setup git
+if [ -z "${GITHUB_TOKEN}"  ]; then
+  echo "GITHUB_TOKEN environment variable not set"
+  exit 1
+fi
+
+echo -e ${GITHUB_PRIVATE_KEY} > ~/.ssh/id_ecdsa
+chmod 0600 ~/.ssh/id_ecdsa
+
+git config --global user.email "${GIT_MAIL}"
+git config --global user.name "${GIT_USER}"
+
+# Update the bosh release repo
+filename=$(basename $(ls s3.suse-buildpacks/*.zip))
+filesize=$(du -b ${filename} | awk '{print $1}')
+checksum=$(sha1sum ${filename} | cut -d' ' -f1)
+
+
+pushd git.buildpack-release
+
+cat << EOF > config/blobs.yml
+---
+${BUILDPACK}-buildpack/${filename}:
+  size: ${filesize}
+  object_id: ${filename}
+  sha: ${checksum}
+EOF
+git commit config/blobs.yml -m "Bump to ${filename}"
+git push origin
+
+commit_id=`git rev-parse HEAD`
+commit_message=`git log --format=%b -n 1`
+popd
+
+
+# Open pull request
+pushd git.scf
+git_branch_name="incorporate_${filename}"
+git checkout -b ${git_branch_name}
+
+# Checkout buildpack submodule
+pushd src/buildpacks/${BUILDPACK}-buildpack-release
+git fetch
+git checkout $commit_id
+popd
+
+# Create bump commit
+git commit src/buildpacks/${BUILDPACK}-buildpack-release -m "Bump ${BUILDPACK} buildpack" -m "${commit_message}"
+git push origin $git_branch_name
+
+pr_message=`echo -e "Bump ${BUILDPACK} buildpack\n\n${commit_message}"`
+export PR_URL=$(hub pull-request -m "${pr_message}")
+popd
--- brats/tasks/prepare-release.sh
+++ brats/tasks/prepare-release.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
+
 set -xe
-
-# Get rid of quotes in the beginning and end
-export GITHUB_PRIVATE_KEY=${GITHUB_PRIVATE_KEY:1:-1}
-
-# Setup git
-if [ -z "${GITHUB_TOKEN}"  ]; then
-  echo "GITHUB_TOKEN environment variable not set"
-  exit 1
-fi
-
-echo -e ${GITHUB_PRIVATE_KEY} > ~/.ssh/id_ecdsa
-chmod 0600 ~/.ssh/id_ecdsa
-
-git config --global user.email "${GIT_MAIL}"
-git config --global user.name "${GIT_USER}"
 
 # Get staging buildpack
 file=`ls git.cf-buildpack-releases/*/*.json | tail -n1`
@@ -70,47 +56,5 @@ zip -r production-buildpack.zip manifest.yml
 
 new_checksum=$(sha1sum production-buildpack.zip | cut -d' ' -f1)
 new_filename=$(echo ${original_filename/-pre-/-} | sed -e "s/[0-9a-f]\{8\}.zip/${new_checksum:0:8}.zip/")
-new_filesize=$(du -b production-buildpack.zip | awk '{print $1}')
 
-mv production-buildpack.zip ${new_filename}
-
-
-# Copy buildpack
-aws s3 cp ${new_filename} s3://${PRODUCTION_BUCKET_NAME}/
-
-
-# Update the bosh release repo
-pushd git.buildpack-release
-cat << EOF > config/blobs.yml
----
-${BUILDPACK}-buildpack/${new_filename}:
-  size: ${new_filesize}
-  object_id: ${new_filename}
-  sha: ${new_checksum}
-EOF
-git commit config/blobs.yml -m "Bump to ${new_filename}"
-git push origin
-
-commit_id=`git rev-parse HEAD`
-commit_message=`git log --format=%b -n 1`
-popd
-
-
-# Open pull request
-pushd git.scf
-git_branch_name="incorporate_${new_filename}"
-git checkout -b ${git_branch_name}
-
-# Checkout buildpack submodule
-pushd src/buildpacks/${BUILDPACK}-buildpack-release
-git fetch
-git checkout $commit_id
-popd
-
-# Create bump commit
-git commit src/buildpacks/${BUILDPACK}-buildpack-release -m "Bump ${BUILDPACK} buildpack" -m "${commit_message}"
-git push origin $git_branch_name
-
-pr_message=`echo -e "Bump ${BUILDPACK} buildpack\n\n${commit_message}"`
-export PR_URL=$(hub pull-request -m "${pr_message}")
-popd
+mv production-buildpack.zip s3-out/${new_filename}
