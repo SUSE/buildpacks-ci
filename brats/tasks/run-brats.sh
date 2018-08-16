a98e974f9bd4e8f64c3de6752063143794b59cc9
Tim Hardeck <thardeck@suse.com>
2018-08-16 10:44:35 +0200

    Fix email notifications
    Signed-off-by: André Duffeck <aduffeck@suse.com>

--- brats/tasks/run-brats.sh
+++ brats/tasks/run-brats.sh
@@ -5,19 +5,21 @@ set -e
 source ci/brats/tasks/cf_login.sh
 
 # make sure that we do not test the git version but the buildpack one
-rm git.buildpack/manifest.yml git.buildpack/VERSION
 
 cd git.buildpack
+# Make sure the manifest and version file from git are not used
+rm manifest.yml VERSION
+
 unzip ../s3.suse-buildpacks-staging/*.zip  manifest.yml VERSION
 
 scripts/brats.sh
 
 if [ $? -ne 0 ]; then
-cat << EOF > mail-output/subject-failed.txt
+cat << EOF > ../mail-output/subject-failed.txt
 [CI] ${BUILDPACK} BRATs have failed
 EOF
-cat << EOF > mail-output/body-failed.txt
-The ${BUILDPACK} BRATs have failed for the following buildpack: $(cat ../s3.suse-buildbacks-staging-*/url)
+cat << EOF > ../mail-output/body-failed.txt
+The ${BUILDPACK} BRATs have failed for the following buildpack: $(cat ../s3.suse-buildbacks-staging/url)
 
 See concourse for more details.
 EOF
