9ee09fba009b12ac00fdaf057f875c21c7a47be4
Tim Hardeck <thardeck@suse.de>
2019-01-03 10:42:37 +0100

    Fix error mail generation for brats

--- brats/tasks/run-brats.sh
+++ brats/tasks/run-brats.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-source ci/brats/tasks/cf_login.sh
+echo "[CI] ${BUILDPACK} BRATs have failed" > mail-output/subject-failed.txt
+
+source ci/brats/tasks/cf_login.sh &> mail-output/body-failed.txt
 
 # Setup git
 git config --global user.email "${GIT_MAIL}"
@@ -11,21 +13,10 @@ git config --global user.name "${GIT_USER}"
 # make sure that we do not test the git version but the buildpack one
 cd git.cf-buildpack
 # Make sure the manifest and version file from git are not used
-rm manifest.yml VERSION
+rm manifest.yml VERSION &> ../mail-output/body-failed.txt
 
-unzip ../s3.suse-buildpacks-staging/*.zip  manifest.yml VERSION
+unzip ../s3.suse-buildpacks-staging/*.zip  manifest.yml VERSION &> ../mail-output/body-failed.txt
 
-git commit manifest.yml VERSION -m "Replace manifest and VERSION by the version to test"
+git commit manifest.yml VERSION -m "Replace manifest and VERSION by the version to test" &> ../mail-output/body-failed.txt
 
-scripts/brats.sh
-
-if [ $? -ne 0 ]; then
-cat << EOF > ../mail-output/subject-failed.txt
-[CI] ${BUILDPACK} BRATs have failed
-EOF
-cat << EOF > ../mail-output/body-failed.txt
-The ${BUILDPACK} BRATs have failed for the following buildpack: $(cat ../s3.suse-buildbacks-staging/url)
-
-See concourse for more details.
-EOF
-fi
+scripts/brats.sh &> ../mail-output/body-failed.txt
