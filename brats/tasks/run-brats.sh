99e6fcd6cf2c7cb234e72fbbe65899f2061e16b9
Tim Hardeck <thardeck@suse.de>
2019-01-03 10:54:43 +0100

    Use tee instead of pipe to show brats output in Concourse

--- brats/tasks/run-brats.sh
+++ brats/tasks/run-brats.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "[CI] ${BUILDPACK} BRATs have failed" > mail-output/subject-failed.txt
 
-source ci/brats/tasks/cf_login.sh &> mail-output/body-failed.txt
+source ci/brats/tasks/cf_login.sh 2>&1 | tee mail-output/body-failed.txt
 
 # Setup git
 git config --global user.email "${GIT_MAIL}"
@@ -13,10 +13,10 @@ git config --global user.name "${GIT_USER}"
 # make sure that we do not test the git version but the buildpack one
 cd git.cf-buildpack
 # Make sure the manifest and version file from git are not used
-rm manifest.yml VERSION &> ../mail-output/body-failed.txt
+rm manifest.yml VERSION 2>&1 | tee ../mail-output/body-failed.txt
 
-unzip ../s3.suse-buildpacks-staging/*.zip  manifest.yml VERSION &> ../mail-output/body-failed.txt
+unzip ../s3.suse-buildpacks-staging/*.zip  manifest.yml VERSION 2>&1 | tee ../mail-output/body-failed.txt
 
-git commit manifest.yml VERSION -m "Replace manifest and VERSION by the version to test" &> ../mail-output/body-failed.txt
+git commit manifest.yml VERSION -m "Replace manifest and VERSION by the version to test" 2>&1 | tee ../mail-output/body-failed.txt
 
-scripts/brats.sh &> ../mail-output/body-failed.txt
+scripts/brats.sh 2>&1 | tee ../mail-output/body-failed.txt
