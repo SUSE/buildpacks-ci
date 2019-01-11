d818f729aafb245df22e2347a9e6a2ed99c9510d
Dimitris Karakasilis <DKarakasilis@suse.com>
2019-01-11 11:51:12 +0200

    Delete all buildpacks in cleanup script
    to let the integration tests work. They are trying to push buildpacks
    under the existing buildpack names but with stack association and they
    fail because our built-in buildpacks are not associated with any stack.

--- brats/tasks/cleanup.sh
+++ brats/tasks/cleanup.sh
@@ -7,5 +7,5 @@ source ci/brats/tasks/cf_login.sh
 # Delete leftover apps
 for app in $(cf apps | grep "cutlass-" | awk '{print $1}'); do cf delete -f $app; done
 
-# Delete leftover buildpacks
-for buildpack in $(cf buildpacks | grep "brats_" | awk '{print $1}'); do cf delete-buildpack -f $buildpack; done
+# Delete all buildpacks (in case there are leftovers)
+for buildpack in $(cf buildpacks | tail -n +4 | awk '{print $1}'); do cf delete-buildpack -f $buildpack; done
