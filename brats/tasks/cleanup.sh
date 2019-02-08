d33c9f521f0afb95c57e899a36d8c657402f86a0
Ettore Di Giacinto <edigiacinto@suse.com>
2019-02-08 15:09:54 +0100

    Delete all apps in cleanup script
    Integration tests generate apps with no prefixes. When integration tests
    fails, applications are left behind, that exhaust the organization
    resource limit.

--- brats/tasks/cleanup.sh
+++ brats/tasks/cleanup.sh
@@ -5,7 +5,7 @@ set -e
 source ci/brats/tasks/cf_login.sh
 
 # Delete leftover apps
-for app in $(cf apps | grep "cutlass-" | awk '{print $1}'); do cf delete -f $app; done
+for app in $(cf apps | awk '{print $1}'); do cf delete -f $app; done
 
 # Delete all buildpacks (in case there are leftovers)
 for buildpack in $(cf buildpacks | tail -n +4 | awk '{print $1}'); do cf delete-buildpack -f $buildpack; done
