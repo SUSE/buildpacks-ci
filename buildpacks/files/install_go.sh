c04ff0dd1e8ad4b0e764db0d8018095153c3effc
Tim Hardeck <thardeck@suse.de>
2019-02-22 16:06:33 +0100

    Change stack to new name in install_go.sh

--- buildpacks/files/install_go.sh
+++ buildpacks/files/install_go.sh
@@ -8,7 +8,7 @@ if [ $CF_STACK == "cflinuxfs2" ]; then
     GO_SHA256="51cab63f3de5e2f75a9036801712e4d7ae9bf226f0b61abce8d784e698148d3b"
 elif [ $CF_STACK == "cflinuxfs3" ]; then
     GO_SHA256="ee770df4e1863ee8e07574cb48e0245b61bec8f118faf6ec3742ea89eb20db28"
-elif [ $CF_STACK == "cfsle15fs" ]; then
+elif [ $CF_STACK == "sle15" ]; then
     GO_SHA256="732874fea9c679e4e6239ffd0d04433f7b72aaef3f3636a2684d9e538e2afb5e"
 elif [ $CF_STACK == "sle12" ]; then
     GO_SHA256="a369350d61414f7a767972e105cc8c3f8ef8e18664165b564538042d1ea944cf"
@@ -26,7 +26,7 @@ mkdir -p $GoInstallDir
 if [ ! -f $GoInstallDir/go/bin/go ]; then
   if [[ "$CF_STACK" =~ cflinuxfs[23] ]]; then
     URL=https://buildpacks.cloudfoundry.org/dependencies/go/go${GO_VERSION}.linux-amd64-${CF_STACK}-${GO_SHA256:0:8}.tar.gz
-  elif [[ "$CF_STACK" == "cfsle15fs" || "$CF_STACK" == "sle12" || "$CF_STACK" == "opensuse42" ]]; then
+  elif [[ "$CF_STACK" == "sle15" || "$CF_STACK" == "sle12" || "$CF_STACK" == "opensuse42" ]]; then
     URL=https://cf-buildpacks.suse.com/dependencies/go/go-${GO_VERSION}-linux-amd64-${CF_STACK}-${GO_SHA256:0:8}.tgz
   fi
 
