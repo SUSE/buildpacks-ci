c4e40c74a1a779f307d15f18a2580b64439214e6
Ettore Di Giacinto <edigiacinto@suse.com>
2019-09-30 16:01:31 +0300

    Use latest cgroup fix from concourse
    https://github.com/concourse/ci/blob/master/tasks/scripts/docker-helpers.sh
    
    Signed-off-by: Dimitris Karakasilis <DKarakasilis@suse.com>

--- brats/tasks/helpers.sh
+++ brats/tasks/helpers.sh
@@ -1,40 +1,83 @@
 #!/bin/bash
 
-function permit_device_control() {
-  local devices_mount_info=$(cat /proc/self/cgroup | grep devices)
+set -e -u
 
-  if [ -z "$devices_mount_info" ]; then
-    # cgroups not set up; must not be in a container
-    echo "Not in a container"
-    return
-  fi
+function sanitize_cgroups() {
+  mkdir -p /sys/fs/cgroup
+  mountpoint -q /sys/fs/cgroup || \
+    mount -t tmpfs -o uid=0,gid=0,mode=0755 cgroup /sys/fs/cgroup
 
-  local devices_subsytems=$(echo $devices_mount_info | cut -d: -f2)
-  local devices_subdir=$(echo $devices_mount_info | cut -d: -f3)
+  mount -o remount,rw /sys/fs/cgroup
 
-  if [ "$devices_subdir" = "/" ]; then
-    # we're in the root devices cgroup; must not be in a container
-    echo "Not in a container"
-    return
-  fi
-
-  cgroup_dir=/tmp/devices-cgroup
-
-  if [ ! -e ${cgroup_dir} ]; then
-    # mount our container's devices subsystem somewhere
-    echo "Creating cgroup folder in ${cgroup_dir}"
-    mkdir ${cgroup_dir}
-  fi
-
-  if ! mountpoint -q ${cgroup_dir}; then
-    if ! mount -t cgroup -o $devices_subsytems none ${cgroup_dir}; then
-        echo "Mounting in ${cgroup_dir}"
-      return 1
+  sed -e 1d /proc/cgroups | while read sys hierarchy num enabled; do
+    if [ "$enabled" != "1" ]; then
+      # subsystem disabled; skip
+      continue
     fi
+
+    grouping="$(cat /proc/self/cgroup | cut -d: -f2 | grep "\\<$sys\\>")"
+    if [ -z "$grouping" ]; then
+      # subsystem not mounted anywhere; mount it on its own
+      grouping="$sys"
+    fi
+
+    mountpoint="/sys/fs/cgroup/$grouping"
+
+    mkdir -p "$mountpoint"
+
+    # clear out existing mount to make sure new one is read-write
+    if mountpoint -q "$mountpoint"; then
+      umount "$mountpoint"
+    fi
+
+    mount -n -t cgroup -o "$grouping" cgroup "$mountpoint"
+
+    if [ "$grouping" != "$sys" ]; then
+      if [ -L "/sys/fs/cgroup/$sys" ]; then
+        rm "/sys/fs/cgroup/$sys"
+      fi
+
+      ln -s "$mountpoint" "/sys/fs/cgroup/$sys"
+    fi
+  done
+
+  if ! test -e /sys/fs/cgroup/systemd ; then
+    mkdir /sys/fs/cgroup/systemd
+    mount -t cgroup -o none,name=systemd none /sys/fs/cgroup/systemd
+  fi
+}
+
+function start_docker() {
+  mkdir -p /var/log
+  mkdir -p /var/run
+
+  sanitize_cgroups
+
+  # check for /proc/sys being mounted readonly, as systemd does
+  if grep '/proc/sys\s\+\w\+\s\+ro,' /proc/mounts >/dev/null; then
+    mount -o remount,rw /proc/sys
   fi
 
-  # permit our cgroup to do everything with all devices
-  # ignore failure in case something has already done this; echo appears to
-  # return EINVAL, possibly because devices this affects are already in use
-  echo a > ${cgroup_dir}${devices_subdir}/devices.allow || true
+  local mtu=$(cat /sys/class/net/$(ip route get 8.8.8.8|awk '{ print $5 }')/mtu)
+  local server_args="--mtu ${mtu}"
+
+  dockerd --data-root /scratch/docker ${server_args} >/tmp/docker.log 2>&1 &
+  echo $! > /tmp/docker.pid
+
+  sleep 1
+
+  until docker info >/dev/null 2>&1; do
+    echo waiting for docker to come up...
+    sleep 1
+  done
+}
+
+function stop_docker() {
+  local pid=$(cat /tmp/docker.pid)
+  if [ -z "$pid" ]; then
+    return 0
+  fi
+
+  kill -TERM $pid
+  wait $pid
 }
