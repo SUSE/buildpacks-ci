81387b31a9156ec61b3358047da0395f4caa0da4
Tim Hardeck <thardeck@suse.com>
2020-04-21 19:17:12 +0200

    Delete stack associated buildpacks

--- brats/tasks/cleanup.sh
+++ brats/tasks/cleanup.sh
@@ -8,7 +8,7 @@ source ci/brats/tasks/cf_login.sh
 for app in $(cf apps | awk '{print $1}'); do cf delete -f $app; done
 
 # Delete all buildpacks (in case there are leftovers)
-for buildpack in $(cf buildpacks | tail -n +4 | awk '{print $1}'); do cf delete-buildpack -f $buildpack; done
+for buildpack in $(cf buildpacks | tail -n +4 | awk '{print $1}'); do cf delete-buildpack -f $buildpack -s sle15; cf delete-buildpack -f $buildpack -s cflinuxfs3; done
 
 # Delete all services 
 for service in $(cf services | tail -n +4 | awk '{print $1}'); do cf delete-service -f $service; done
